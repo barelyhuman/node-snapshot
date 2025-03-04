b8e6478a29a6048c41669708da98d22d99a02248
reaper <ahoy@barelyhuman.dev>
2025-03-04 19:36:00 +0530

    fix: typo

--- src/snapshot.js
+++ src/snapshot.js
@@ -121,7 +121,7 @@ function writeSnapshot(value, file, name) {
   if (!existsSync(file)) {
     let data = ''
     data += '\n\n'
-    data += `exports.[${JSON.stringify(name)}] = \`${formatValue(value)}\``
+    data += `exports[${JSON.stringify(name)}] = \`${formatValue(value)}\``
     mkdirSync(dirname(file), { recursive: true })
     writeFileSync(file, data, 'utf8')
     return
