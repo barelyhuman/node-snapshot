41d4969a2d6ba44dba8d862f3da26be6b0119364
reaper <ahoy@barelyhuman.dev>
2025-03-04 23:45:14 +0530

    chore: update snapshots with specific `node` versin

--- snapshots/tests/mocha-suite.test.snap.cjs
+++ snapshots/tests/mocha-suite.test.snap.cjs
@@ -1,22 +1,3 @@
-exports["yo > should yo 1"] = `"make it rain"`
-
-exports["yo 2 > it yo 2 1"] = `Object {
-  "name": 3,
-}`
-
-exports["yo 2 > it yo 2 2"] = `Object {
-  "name": 1,
-}`
-
-exports["for > lols 1"] = `Array [
-  Object {
-    "name": 1,
-  },
-  Object {
-    "name": 2,
-  },
-]`
-
 exports["should yo 1"] = `"make it rain"`
 
 exports["it yo 2 1"] = `Object {
@@ -36,9 +17,9 @@ exports["lols 1"] = `Array [
   },
 ]`
 
-exports["arrays > should handle empty arrays 1"] = `Array []`
+exports["should handle empty arrays 1"] = `Array []`
 
-exports["arrays > should handle nested arrays 1"] = `Array [
+exports["should handle nested arrays 1"] = `Array [
   Array [
     1,
     2,
@@ -49,7 +30,7 @@ exports["arrays > should handle nested arrays 1"] = `Array [
   ],
 ]`
 
-exports["objects > should handle nested objects 1"] = `Object {
+exports["should handle nested objects 1"] = `Object {
   "user": Object {
     "name": "test",
     "profile": Object {
@@ -58,13 +39,13 @@ exports["objects > should handle nested objects 1"] = `Object {
   },
 }`
 
-exports["objects > should handle null values 1"] = `Object {
+exports["should handle null values 1"] = `Object {
   "value": null,
 }`
 
-exports["primitives > should handle numbers 1"] = `42`
+exports["should handle numbers 1"] = `42`
 
-exports["primitives > should handle booleans 1"] = `true`
+exports["should handle booleans 1"] = `true`
 
-exports["primitives > should handle booleans 2"] = `false`
+exports["should handle booleans 2"] = `false`
 
